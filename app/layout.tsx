b5b8b281b81ed6c2a5911e6b68c6cf837e7f8fd8
jianyi <drinking.cn@gmail.com>
2024-04-05 17:21:27 -0600

    test env

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
+
+const simpleToken = process.env.SIMPLE_TOKEN ?? "";
+
 
 export const metadata: Metadata = {
   title: "HomeDepot Workshop Monitor",
@@ -16,6 +19,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log(simpleToken);
   return (
     <html lang="en">
       <body className={montserrat.className}>

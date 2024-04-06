7b47030bda9e53d44f773c94b7dca7c7449a76c3
jianyi <drinking.cn@gmail.com>
2024-04-05 21:06:22 -0600

    delete file

--- app/_db/configs.ts
+++
@@ -1,5 +0,0 @@
-// 1st, old url => 'https://www.homedepot.ca/homedepotcacommercewebservices/v2/homedepotca/workshops/store/7063?lang=en';
-// 2nd, working url => https://www.homedepot.ca/api/workshopsvc/v1/workshops/all?storeId=7063&lang=en';
-export const apiBaseUrl: string = "https://www.homedepot.ca/api/workshopsvc/v1/workshops/all?storeId=";
-export const registerUrl: string = "https://www.homedepot.ca/workshops";
-
